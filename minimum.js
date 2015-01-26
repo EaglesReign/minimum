7967b9a1e8d285cada4460e7ac7a1cefefe0374a
Phillip <phv9495@saintjoe.edu>
2015-01-26 12:16:12 -0500

    Add comments to the homework

--- minimum.js
+++ minimum.js
@@ -1,8 +1,12 @@
 //Phil Hofer
 //Ch. 3 mimimum.js
+//This program takes two arguments and returns the minimum value of the two.
+//Updated 1/26/2015 with comments
 
+//declare the variables needed for the min function
 var x, y;
 
+//This function returns the minimum value between two arguments
 function min(x, y) {
   if (x < y) {
     return x;
@@ -11,4 +15,5 @@ function min(x, y) {
   }  
 }
 
+//Print out the minimum between 14 and 11
 console.log("The mimimum is: " + min(14,11));
