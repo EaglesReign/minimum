505031de011b06a5bffd9caf9747e9a551f6ea3a
Phillip <phv9495@saintjoe.edu>
2015-01-26 00:43:38 -0500

    This is a working version of the Ch3 minimum program.

---
+++ minimum.js
@@ -0,0 +1,14 @@
+//Phil Hofer
+//Ch. 3 mimimum.js
+
+var x, y;
+
+function min(x, y) {
+  if (x < y) {
+    return x;
+  } else {
+    return y;
+  }  
+}
+
+console.log("The mimimum is: " + min(14,11));
